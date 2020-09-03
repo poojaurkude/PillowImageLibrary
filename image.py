3bb2b8cfd738b2414f756043a0cd5b35111e313b
pooja <pooja11urkude@gmail.com>
2020-09-03 18:38:19 +0530

    version1

---
+++ image.py
@@ -0,0 +1,72 @@
+# from PIL import Image
+# img = Image.open('poke.jpg')
+# print(img.format, img.size, img.mode)
+
+
+
+# from PIL import Image, ImageFilter
+# img = Image.open('pikachu.jpg')
+# filtered_img = img.filter(ImageFilter.BLUR)
+# filtered_img.save("blur.png",'png')
+
+# from PIL import Image, ImageFilter
+# img = Image.open('pikachu.jpg')
+# filtered_img = img.filter(ImageFilter.SMOOTH)
+# filtered_img.save("blur.png",'png')
+
+# from PIL import Image, ImageFilter
+# img = Image.open('pikachu.jpg')
+# filtered_img = img.filter(ImageFilter.SHARPEN)
+# filtered_img.save("blur.png",'png')
+
+
+
+# from PIL import Image, ImageFilter
+# img = Image.open('pikachu.jpg')
+# filtered_img = img.convert('L')
+# filtered_img.save("grey.png",'png')
+# filtered_img.show()
+
+
+
+# from PIL import Image, ImageFilter
+# img = Image.open('pikachu.jpg')
+# filtered_img = img.convert('L')
+# pika = filtered_img.rotate(180)
+# pika.save("pika.png",'png')
+
+
+
+
+# from PIL import Image, ImageFilter
+# img = Image.open('pikachu.jpg')
+# filtered_img = img.convert('L')
+# resize = filtered_img.resize((300,300))
+# resize.save("resize.png",'png')
+
+
+
+# from PIL import Image, ImageFilter
+# img = Image.open('poke1.jpg')
+# filtered_img = img.convert('L')
+# box = (100,100,200,200)
+# crop = filtered_img.crop(box)
+# crop.save("crop.png",'png')
+
+
+
+# from PIL import Image, ImageFilter
+# img = Image.open('poke.jpg')
+# img.thumbnail((400,200))
+# img.save('thumbnail.jpg')
+
+# # print(img.size)
+
+
+
+
+
+
+
+
+
